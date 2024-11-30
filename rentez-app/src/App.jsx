95fe81a9572fed6071bda4620c839f0df05b3400
Uriel <leirualexisparantar@gmail.com>
2024-11-30 13:23:28 +0800

    added listpage

--- rentez-app/src/App.jsx
+++ rentez-app/src/App.jsx
@@ -1,10 +1,12 @@
 import "./layout.scss";
 import Navbar from "./components/navbar/Navbar";
+import SinglePage from "./routes/singlePage/singlePage";
 
 function App() {
   return (
     <div className="layout">
       <Navbar />
+      <SinglePage />
     </div>
   );
 }
---
+++ rentez-app/src/dummydata/dummydata.js
@@ -0,0 +1,66 @@
+export const listData = [
+  {
+    id: 1,
+    title: "Cozy Apartment in Downtown",
+    img: "https://via.placeholder.com/150",
+    bedroom: 2,
+    bathroom: 1,
+    price: 1200,
+    address: "123 Main Street, City Center",
+    latitude: 37.7749,
+    longitude: -122.4194,
+  },
+  {
+    id: 2,
+    title: "Spacious Suburban House",
+    img: "https://via.placeholder.com/150",
+    bedroom: 4,
+    bathroom: 3,
+    price: 2500,
+    address: "456 Elm Street, Suburbia",
+    latitude: 34.0522,
+    longitude: -118.2437,
+  },
+  {
+    id: 3,
+    title: "Modern Condo Near the Park",
+    img: "https://via.placeholder.com/150",
+    bedroom: 3,
+    bathroom: 2,
+    price: 1800,
+    address: "789 Pine Street, Near Park",
+    latitude: 40.7128,
+    longitude: -74.006,
+  },
+];
+
+export const singlePostData = {
+  images: [
+    "https://www.decorilla.com/online-decorating/wp-content/uploads/2024/07/Expert-decorating-for-a-small-living-room-by-Decorilla-scaled.jpg",
+    "https://www.ikea.com/ext/ingkadam/m/b188de628d74f5b/original/PH199271.jpg",
+    "https://optimise2.assets-servd.host/thick-groundhog/production/assets/blog/Modern-Kitchen-Trend.jpg?w=1200&h=630&q=82&auto=format&fit=crop&dm=1655129886&s=944383cb01651f8a63ad163f0775c62a",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvYuZYTDHcMw28NyHdJNRXJb_dvdhYfGZK-w&s",
+  ],
+  title: "Cozy Apartment in Downtown",
+  bedrooms: 3,
+  bathrooms: 2,
+  size: "1500 sqft",
+  latitude: 37.7749,
+  longitude: -122.4194,
+  price: 1800,
+  city: "San Francisco",
+  address: "123 Main Street, City Center",
+  school: "Central High School (0.5 miles)",
+  bus: "Main Street Bus Stop (0.2 miles)",
+  restaurant: "Joe's Diner (0.3 miles)",
+  description: `This modern and spacious apartment is located in the heart of the city,
+      offering easy access to schools, public transport, and popular restaurants.
+      With three well-lit bedrooms, a large living area, and a modern kitchen,
+      this home is perfect for families or professionals seeking convenience and comfort.`,
+};
+
+export const userData = {
+  id: 1,
+  name: "John Doe",
+  img: "https://hips.hearstapps.com/hmg-prod/images/snoop_dogg_photo_by_estevan_oriol_archive_photos_getty_455616412.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*",
+};
