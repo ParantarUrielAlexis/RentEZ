51b1b55287d2fcc67230170cc580aeffb6a4530d
Uriel <leirualexisparantar@gmail.com>
2024-12-07 08:55:25 +0800

    Add data fetching to App.jsx; reformat multiple files
    - Updated .gitignore to include dist-ssr directory.
    - Added useEffect and axios to App.jsx for API data fetching.
    - Refactored Slider.jsx with no logical changes.
    - Reformatted slider.scss, dummydata.js, layout.scss, singlePage.jsx, and singlePage.scss for better readability.

--- rentez-app/src/App.jsx
+++ rentez-app/src/App.jsx
@@ -1,8 +1,21 @@
 import "./layout.scss";
 import Navbar from "./components/navbar/Navbar";
 import SinglePage from "./routes/singlePage/singlePage";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("https://localhost:7183/WeatherForecast")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div className="layout">
       <Navbar />
