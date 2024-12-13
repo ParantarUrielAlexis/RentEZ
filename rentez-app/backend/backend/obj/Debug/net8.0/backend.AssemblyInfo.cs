43b33612a88a970974cd5b15c64987e69c633382
Kent Espia <kentvirgil.espia@cit.edu>
2024-12-13 14:26:09 +0800

    removed image

--- rentez-app/backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ rentez-app/backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2c772ab185312b22c5cfd6c066e72248f29965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5cb39f3f9a8bbcaf9acaf62576bbf6ccad4e5b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
