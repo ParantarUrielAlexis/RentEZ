1fc066ac536f6fb6c6538772f6a0b52b16baf5f2
Kent Espia <kentvirgil.espia@cit.edu>
2024-12-15 19:03:39 +0800

    idk what this is

--- rentez-app/backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ rentez-app/backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38968d2cb0c9d343a7860caad65dc07306e950d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e67b3082ff3026e2e413702dfe7de1c9b058deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
